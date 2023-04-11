cf4e84ecf0986768f171dc87e14e01280fa1b287
Ayoub Aitmansour <ayoub.atm000@gmail.com>
2023-04-10 17:43:17 -0700

    4 Isalpha

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: interger to check
+ * Return: (1) if charcter is lower and (0) if not.
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
